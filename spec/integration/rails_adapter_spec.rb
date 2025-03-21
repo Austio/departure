10331bb38b4d4f84cdf85921492f140a0e73513a
Austin Story <lonnieastory@gmail.com>
2025-03-21 14:42:40 -0700

    Document new feature to Patch ActiveRecord versions 7.1 and 7.2 to workaround with_advisory_lock issue

--- spec/integration/rails_adapter_spec.rb
+++ spec/integration/rails_adapter_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe Departure::RailsAdapter, integration: true do
-  describe "advisory_lock patch" do
+  describe 'advisory_lock patch' do
     before(:each) do
       # We have to force a reconnection in order to get a migration error when we switch adapters
       establish_mysql_connection
@@ -12,13 +12,14 @@ RSpec.describe Departure::RailsAdapter, integration: true do
       migration_context.run(:up, 1)
     end
 
-    it "runs migrations without throwing an ActiveRecord::ConcurrentMigration Error" do
+    it 'runs migrations without throwing an ActiveRecord::ConcurrentMigration Error' do
       establish_mysql_connection
 
       expect { run_a_migration }.not_to raise_error
     end
 
-    it "throws an exception when we are in rails 7.1 and have the patch disabled", activerecord_compatibility: "> 7.1" do
+    it 'throws an exception when we are in rails 7.1 and have the patch disabled',
+       activerecord_compatibility: '> 7.1' do
       Departure.configure do |config|
         config.disable_rails_advisory_lock_patch = true
       end
@@ -29,5 +30,3 @@ RSpec.describe Departure::RailsAdapter, integration: true do
     end
   end
 end
-
-
